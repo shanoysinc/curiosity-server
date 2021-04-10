1bea69b1286a27770d722eece0a48e7b9743ba8b
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2021-04-10 15:31:47 -0800

    change user token interface

--- src/middleware/checkAuth.ts
+++ src/middleware/checkAuth.ts
@@ -1,28 +1,29 @@
 import jsonwebtoken from "jsonwebtoken";
 import { RequestHandler } from "express";
 import { User } from "../entity/User";
-interface Token {
-	id: number;
-	email: string;
-	iat: number;
+
+export interface Token {
+  id: number;
+  userInitials: string;
+  iat: number;
 }
 export const checkAuth: RequestHandler = async (req, res, next) => {
-	const tokenFromReq = req.headers.authorization.split(" ")[1] || "";
+  const tokenFromReq = req.headers.authorization.split(" ")[1] || "";
 
-	const token = jsonwebtoken.verify(
-		tokenFromReq,
-		process.env.TOKEN_SECRET
-	) as Token;
+  const token = jsonwebtoken.verify(
+    tokenFromReq,
+    process.env.TOKEN_SECRET
+  ) as Token;
 
-	if (!token) {
-		return res.send({ message: "unauthorize access!" });
-	}
+  if (!token) {
+    return res.send({ message: "unauthorize access!" });
+  }
 
-	const { id, email } = token;
-	const user = await User.findOne({ email });
-	if (!user) {
-		return res.send({ message: "unauthorize access!" });
-	}
-	req.user = { id, email };
-	next();
+  const { id, userInitials } = token;
+  const user = await User.findOne({ id });
+  if (!user) {
+    return res.send({ message: "unauthorize access!" });
+  }
+  req.user = { id, userInitials };
+  next();
 };
