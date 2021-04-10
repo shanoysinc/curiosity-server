3e21674780722fa532ad817b542ce46044d433c0
Shanoy Sinclair <shanoysinc@users.noreply.github.com>
2021-04-10 15:30:30 -0800

    modify dbconnect to change dboptions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import "reflect-metadata";
-import { createConnection } from "typeorm";
 import dotenv from "dotenv";
 dotenv.config();
+import "reflect-metadata";
+import { connect2Database, getDbOptions } from "./db/db";
 import express from "express";
 import helmet from "helmet";
 import compression from "compression";
@@ -14,30 +14,34 @@ import commentRouter from "./routes/comment/comment.router";
 import answerVoteRouter from "./routes/vote/answerVote.router";
 import commentVoteRouter from "./routes/vote/commentVote.router";
 import questionRouter from "./routes/question/question.router";
+import authRouter from "./routes/auth/auth.router";
 
-createConnection()
-	.then(async (connection) => {
-		const PORT = process.env.PORT || 3000;
+export default connect2Database(getDbOptions)
+  .then(async (connection) => {
+    const PORT = process.env.PORT || 4000;
 
-		const app = express();
-		app.use(cors());
-		app.use(helmet());
-		app.use(compression());
+    const app = express();
+    app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+    app.use(helmet());
+    app.use(compression());
 
-		app.use(express.json());
-		app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
 
-		app.use("/api/user", userRouter);
-		app.use("/api/news-feed", checkAuth, newsFeedRouter);
+    app.use("/api/auth", authRouter);
 
-		app.use("/api/question", checkAuth, questionRouter);
+    app.use("/api/user", checkAuth, userRouter);
 
-		app.use("/api/answers", checkAuth, AnswerRouter);
-		app.use("/api/vote", checkAuth, answerVoteRouter);
+    app.use("/api/news-feed", checkAuth, newsFeedRouter);
 
-		app.use("/api/comments", checkAuth, commentRouter);
-		app.use("/api/vote", checkAuth, commentVoteRouter);
+    app.use("/api/question", checkAuth, questionRouter);
 
-		app.listen(PORT, () => console.log("serving runnin on port", PORT));
-	})
-	.catch((error) => console.log(error));
+    app.use("/api/answers", checkAuth, AnswerRouter);
+    app.use("/api/vote", checkAuth, answerVoteRouter);
+
+    app.use("/api/comments", checkAuth, commentRouter);
+    app.use("/api/vote", checkAuth, commentVoteRouter);
+
+    app.listen(PORT, () => console.log("serving runnin on port", PORT));
+  })
+  .catch((error) => console.log(error));
